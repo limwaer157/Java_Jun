4fe827ef52153ba8cd7ced4932110e46e0fe32cd
Denis <Limwaer157@gmail.com>
2023-12-27 15:12:15 -0500

    comp_3

--- src/main/java/Home_Work/Seminar_2/Thrid_task.java
+++ src/main/java/Home_Work/Seminar_2/Thrid_task.java
@@ -1,4 +1,16 @@
 package Home_Work.Seminar_2;
-
+//Написать функцию, возвращающую истину, если в переданном
+// массиве есть два соседних элемента, с нулевым значением.
 public class Thrid_task {
+
+    public static void main(String[] args) {
+        int [] array = {123,1,2,3,4,5,0};
+        System.out.println(find(array));
+    }
+    public static boolean find(int[]array){
+        for (int i = 0; i < array.length-1; i++) {
+            if (array[i] == 0 && array[i+1] ==0) return true;
+        }
+        return false;
+    }
 }
